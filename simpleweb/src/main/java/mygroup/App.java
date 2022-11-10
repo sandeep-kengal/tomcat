455a614e38dbc9e1bac765c7b61eec2b616115e8
Ubuntu <ubuntu@ip-172-31-5-244.ap-south-1.compute.internal>
2022-11-10 09:03:29 +0000

    warfile

---
+++ simpleweb/src/main/java/mygroup/App.java
@@ -0,0 +1,15 @@
+package mygroup;
+
+/**
+ * Hello world !
+ * welcome
+ *
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
